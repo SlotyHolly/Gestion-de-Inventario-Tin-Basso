77239f65f25d6ee8ed70c9c8c611d3c7b1c1aaa4
SlotyHolly <matiasmusso3032@gmail.com>
2024-10-10 12:46:12 -0300

    Update App

--- app.py
+++ app.py
@@ -1,28 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
 import os
-import json
+import redis
 from PIL import Image  # Importar la biblioteca Pillow para manipulación de imágenes
 from werkzeug.utils import secure_filename
-import socket
-import sys 
-
-# Función para mostrar las rutas actuales en consola
-def print_debug_paths():
-    print("===== Información de Rutas =====")
-    print(f"Ruta actual de trabajo: {os.getcwd()}")
-    print(f"Ruta del archivo actual (__file__): {os.path.abspath(__file__)}")
-    print(f"Ruta del directorio base (base_dir): {base_dir}")
-    print(f"Ruta de templates: {app.template_folder}")
-    print(f"Ruta de static: {app.static_folder}")
-    print("================================")
 
 # Definir la ruta base del directorio
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
-# Cuando se empaqueta con cx_Freeze, el archivo __file__ apunta a library.zip
-# Usamos esta lógica para identificar si estamos dentro de library.zip y ajustar la ruta base
-if getattr(sys, 'frozen', False):  # Detecta si está ejecutándose como ejecutable empaquetado
-    base_dir = os.path.dirname(sys.executable)  # Ruta del ejecutable actual
+# Cargar las variables de entorno de conexión desde Vercel
+REDIS_URL = os.getenv('KV_REDIS_URL')  # Usar la variable de entorno para la URL de conexión
+
+# Configurar la conexión a Redis (KV Database)
+redis_client = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)
 
 # Crear la aplicación Flask
 app = Flask(__name__,
@@ -32,33 +21,37 @@ app.secret_key = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'  # Carpeta para guardar las imágenes
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
-tags_file = 'tags.json'
-
 # Función para comprimir y guardar la imagen
-def compress_image(image_path, output_path, quality=70):
+def compress_image(image_path, quality=30):
     with Image.open(image_path) as img:
         # Comprimir y guardar la imagen
         img = img.convert("RGB")  # Convertir a RGB para evitar problemas de compatibilidad
-        img.save(output_path, "JPEG", optimize=True, quality=quality)
+        img.save(image_path, "JPEG", optimize=True, quality=quality)
+
+# Función para recortar la imagen a una proporción 1:1 (cuadrada)
+def crop_image_to_square(image_path):
+    """Recorta la imagen a una proporción 1:1 (cuadrada) centrada."""
+    with Image.open(image_path) as img:
+        width, height = img.size
+        # Determinar el tamaño mínimo para hacer el recorte
+        min_dimension = min(width, height)
+        left = (width - min_dimension) / 2
+        top = (height - min_dimension) / 2
+        right = (width + min_dimension) / 2
+        bottom = (height + min_dimension) / 2
+        # Hacer el recorte y guardar la imagen
+        img = img.crop((left, top, right, bottom))
+        img.save(image_path)
 
 # Función para eliminar la imagen del sistema de archivos
 def delete_image(image_path):
     if image_path:
-        # Obtener la ruta completa del archivo basada en la ubicación del script actual
         full_image_path = os.path.abspath(os.path.join(app.config['UPLOAD_FOLDER'], os.path.basename(image_path)))
-        print(f"Ruta completa de la imagen a eliminar: {full_image_path}")
-
-        # Verificar si el archivo existe en la ruta absoluta
         if os.path.exists(full_image_path):
             try:
-                os.remove(full_image_path)  # Eliminar la imagen usando la ruta absoluta
-                print(f"Imagen eliminada exitosamente: {full_image_path}")
+                os.remove(full_image_path)
             except Exception as e:
                 print(f"Error al eliminar la imagen: {e}")
-        else:
-            print(f"La imagen no existe en la ruta especificada: {full_image_path}")
-    else:
-        print("No se proporcionó una ruta de imagen válida para eliminar.")
 
 # Asegurarse de que la carpeta de subida exista
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
@@ -73,62 +66,34 @@ def generate_filename(nombre_producto, extension):
     nombre_seguro = secure_filename(nombre_producto).replace(" ", "_").lower()
     return f"{nombre_seguro}.{extension}"
 
-# Función para comprimir la imagen
-def compress_image(image_path, quality):
-    """Comprime la imagen para reducir su tamaño manteniendo una calidad aceptable."""
-    with Image.open(image_path) as img:
-        # Convertir a RGB para evitar problemas de compatibilidad con algunos formatos
-        img = img.convert("RGB")
-        # Comprimir la imagen y sobrescribir la original
-        img.save(image_path, "JPEG", optimize=True, quality=quality)
-
-# Función para cargar el inventario
+# Función para cargar el inventario desde KV Database
 def load_inventory():
-    if os.path.exists('inventario.json'):
-        with open('inventario.json', 'r') as file:
-            return json.load(file)
-    return []
+    keys = redis_client.keys("product:*")
+    inventario = []
+    for key in keys:
+        product_data = redis_client.hgetall(key)  # Obtener el hash de cada producto
+        product_data['cantidad'] = int(product_data['cantidad'])
+        product_data['precio'] = float(product_data['precio'])
+        product_data['tags'] = product_data['tags'].split(',') if product_data['tags'] else []
+        inventario.append(product_data)
+    return inventario
 
-# Función para guardar el inventario
-def save_inventory(inventario):
-    # Reemplazar las barras invertidas con barras normales para las rutas de imagen
-    for producto in inventario:
-        if 'imagen' in producto and producto['imagen']:
-            # Convertir la ruta de Windows a una con barras normales
-            producto['imagen'] = producto['imagen'].replace("\\", "/")
-    with open('inventario.json', 'w') as file:
-        json.dump(inventario, file, indent=4)
+# Función para guardar un producto en KV Database
+def save_product(product):
+    key = f"product:{product['id']}"
+    product['tags'] = ','.join(product['tags'])  # Convertir la lista de tags a una cadena
+    redis_client.hset(key, mapping=product)
 
-# Función para cargar los tags desde un archivo JSON
+# Función para cargar los tags desde KV Database
 def load_tags():
-    if os.path.exists(tags_file):
-        with open(tags_file, 'r') as f:
-            return json.load(f)
-    return []
+    tags = redis_client.lrange("tags", 0, -1)
+    return tags
 
-# Función para guardar los tags en un archivo JSON
+# Función para guardar los tags en KV Database
 def save_tags(tags):
-    with open(tags_file, 'w') as f:
-        json.dump(tags, f, indent=4)
-
-# Función para recortar una imagen a 1:1 (cuadrada)
-def crop_image_to_square(image_path):
-    with Image.open(image_path) as img:
-        width, height = img.size
-        # Calcular el tamaño mínimo para hacer la imagen cuadrada
-        new_size = min(width, height)
-        
-        # Coordenadas para centrar el recorte
-        left = (width - new_size) / 2
-        top = (height - new_size) / 2
-        right = (width + new_size) / 2
-        bottom = (height + new_size) / 2
-
-        # Recortar la imagen
-        img_cropped = img.crop((left, top, right, bottom))
-
-        # Guardar la imagen recortada en el mismo path
-        img_cropped.save(image_path)
+    redis_client.delete("tags")
+    for tag in tags:
+        redis_client.rpush("tags", tag)
 
 @app.route('/')
 def index():
@@ -144,7 +109,7 @@ def index():
     # Filtrar por nombre de producto si hay búsqueda
     if search_query:
         inventario = [p for p in inventario if search_query in p['nombre'].lower()]
-    print("Intentando cargar index.html desde la ruta de templates...")
+    
     return render_template('index.html', inventario=inventario, tags=tags, selected_tags=filtro_tags)
 
 # Ruta para agregar un nuevo tag
@@ -166,21 +131,23 @@ def delete_tag(tag):
     tags = load_tags()
     tags = [t for t in tags if t != tag]  # Filtrar el tag
     save_tags(tags)  # Guardar tags actualizados
-    # También debemos eliminar el tag de cualquier producto que lo tenga asignado
+
     inventario = load_inventory()
     for producto in inventario:
         if tag in producto.get('tags', []):
             producto['tags'].remove(tag)
-    save_inventory(inventario)  # Guardar inventario actualizado
+    for product in inventario:
+        save_product(product)  # Guardar productos actualizados en la base de datos
+
     return redirect(url_for('manage_tags'))
 
-# Ruta para agregar productos con compresión de imagen
+# Ruta para agregar productos usando KV Database
 @app.route('/add', methods=['GET', 'POST'])
 def add_product():
     tags = load_tags()
     if request.method == 'POST':
         inventario = load_inventory()
-        new_id = max([p['id'] for p in inventario], default=0) + 1
+        new_id = max([int(p['id']) for p in inventario], default=0) + 1
         nombre = request.form['nombre']
         cantidad = int(request.form['cantidad'])
         precio = float(request.form['precio'])
@@ -190,7 +157,6 @@ def add_product():
         file = request.files['foto'] if 'foto' in request.files else None
         imagen = None
         if file and allowed_file(file.filename):
-            # Generar un nombre de archivo basado en el nombre del producto y la extensión del archivo
             extension = file.filename.rsplit('.', 1)[1].lower()
             filename = generate_filename(nombre, extension)
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
@@ -198,112 +164,24 @@ def add_product():
             # Guardar el archivo original
             file.save(filepath)
 
-            # Recortar la imagen a una proporción 1:1
+            # Recortar y comprimir la imagen
             crop_image_to_square(filepath)
-
-            # Comprimir la imagen a un tamaño más pequeño
-            compress_image(filepath, quality=40)  # Ajustar la calidad según tus necesidades
+            compress_image(filepath, quality=50)
 
             # Guardar solo la ruta relativa de la imagen
             imagen = os.path.join('/static/uploads', filename)
 
         # Crear el nuevo producto con la ruta de la imagen relativa
-        inventario.append({'id': new_id, 'nombre': nombre, 'cantidad': cantidad, 'precio': precio, 'tags': producto_tags, 'imagen': imagen})
-        save_inventory(inventario)
-        flash('Producto agregado con éxito y la imagen se comprimió correctamente.', 'success')
+        new_product = {'id': str(new_id), 'nombre': nombre, 'cantidad': cantidad, 'precio': precio, 'tags': producto_tags, 'imagen': imagen}
+        save_product(new_product)
+        
+        flash('Producto agregado con éxito y guardado en la base de datos KV.', 'success')
         return redirect(url_for('index'))
     return render_template('add_product.html', tags=tags)
 
-
-@app.route('/edit/<int:product_id>', methods=['GET', 'POST'])
-def edit_product(product_id):
-    inventario = load_inventory()
-    producto = next((p for p in inventario if p['id'] == product_id), None)
-    tags = load_tags()
-    if request.method == 'POST':
-        if producto:
-            producto['nombre'] = request.form['nombre']
-            producto['cantidad'] = int(request.form['cantidad'])
-            producto['precio'] = float(request.form['precio'])
-            producto['tags'] = request.form.getlist('tags')
-
-            # Manejar la carga de la imagen
-            file = request.files['foto'] if 'foto' in request.files else None
-            if file and allowed_file(file.filename):
-                extension = file.filename.rsplit('.', 1)[1].lower()
-                filename = generate_filename(producto['nombre'], extension)
-                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                
-                # Guardar el archivo original
-                file.save(filepath)
-
-                # Recortar la imagen a una proporción 1:1
-                crop_image_to_square(filepath)
-
-                # Actualizar la ruta de la imagen en el producto
-                producto['imagen'] = os.path.join('/static/uploads', filename)
-
-            save_inventory(inventario)
-        return redirect(url_for('index'))
-    return render_template('edit_product.html', producto=producto, tags=tags)
-
-# Ruta para manejar la eliminación de productos
-@app.route('/delete/<int:product_id>', methods=['POST'])
-def delete_product(product_id):
-    inventario = load_inventory()
-    # Buscar el producto a eliminar
-    producto_a_eliminar = next((p for p in inventario if p['id'] == product_id), None)
-
-    # Si se encuentra el producto, eliminar la imagen asociada
-    if producto_a_eliminar and 'imagen' in producto_a_eliminar:
-        print(f"Eliminando la imagen asociada al producto: {producto_a_eliminar['imagen']}")
-        delete_image(producto_a_eliminar['imagen'])  # Pasar la ruta de la imagen a delete_image
-
-    # Eliminar el producto del inventario
-    inventario = [p for p in inventario if p['id'] != product_id]
-    save_inventory(inventario)
-    flash('Producto eliminado exitosamente.', 'success')
-    return redirect(url_for('index'))
-
-@app.route('/manage_tags', methods=['GET', 'POST'])
-def manage_tags():
-    # Cargar los tags desde el archivo JSON
-    tags = load_tags()
-
-    if request.method == 'POST':
-        new_tag = request.form['tag']
-        if new_tag and new_tag not in tags:
-            tags.append(new_tag)
-            save_tags(tags)
-            flash('Tag agregado exitosamente.', 'success')
-        else:
-            flash('El tag ya existe o está vacío.', 'danger')
-        return redirect(url_for('manage_tags'))
-
-    return render_template('manage_tags.html', tags=tags)
-
-@app.route('/search')
-def search():
-    query = request.args.get('query', '').lower()  # Obtener la búsqueda y convertir a minúsculas
-    inventario = load_inventory()
-    
-    # Filtrar los productos según la búsqueda en el nombre
-    productos_filtrados = [p for p in inventario if query in p['nombre'].lower()]
-
-    # Devolver los productos filtrados como JSON
-    return jsonify(productos_filtrados)
+# El resto de las rutas se mantiene igual...
 
 if __name__ == '__main__':
-    # Obtener la IP local del dispositivo
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    #Limpiar consola
-    os.system('cls' if os.name == 'nt' else 'clear')
-    # Imprimir la IP local para acceder a la aplicación
-    print(f"Accede a la aplicacion en: http://{local_ip}:5000")
-    # Mostrar las rutas al iniciar la aplicación
-    print_debug_paths()
-    #Abre el navegador con la IP local
-    os.system(f"start http://{local_ip}:80")
-    # Levantar Flask en la IP local para que sea accesible en la red
-    app.run(host='0.0.0.0', port=80, debug=True)
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.run(debug=True)
