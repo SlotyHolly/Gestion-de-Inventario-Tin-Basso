421142517cfe7ed112aaef6c789717140e46a3ce
SlotyHolly <matiasmusso3032@gmail.com>
2024-10-11 16:58:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def edit_product(product_id):
         product.nombre = nombre
         product.cantidad = cantidad
         product.precio = precio
-
+        Flask('Los tags seleccionados son:', selected_tags)
         # Actualizar los tags utilizando la nueva función
         update_product_tags(product_id, selected_tags)
 
